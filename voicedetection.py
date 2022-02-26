13b498cbff55b2aa4558446667946e610a31c76f
Harvey <hcrutland@mail.com>
2022-02-26 21:38:15 +0000

    first commit

---
+++ voicedetection.py
@@ -0,0 +1,137 @@
+import random
+import time
+import speech_recognition as sr
+
+
+def recognize_speech_from_mic(recognizer, microphone):
+    """Transcribe speech from recorded from `microphone`.
+
+    Returns a dictionary with three keys:
+    "success": a boolean indicating whether or not the API request was
+               successful
+    "error":   `None` if no error occured, otherwise a string containing
+               an error message if the API could not be reached or
+               speech was unrecognizable
+    "transcription": `None` if speech could not be transcribed,
+               otherwise a string containing the transcribed text
+    """
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+
+
+
+COCO_INSTANCE_CATEGORY_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]   
+
+
+if __name__ == "__main__":
+    # set the list of words, maxnumber of guesses, and prompt limit
+    WORDS = ["trigger"]
+    # NUM_GUESSES = 3
+    # PROMPT_LIMIT = 5
+
+    # create recognizer and mic instances
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+
+    # get a random word from the list
+    word = WORDS
+
+    # format the instructions string
+    instructions = (
+        "waiting")
+
+    # show instructions and wait 3 seconds before starting the game
+    print(instructions)
+
+
+    
+
+
+    
+    
+    while(1):
+        input()
+        guess = recognize_speech_from_mic(recognizer, microphone)
+        if guess["transcription"]:
+            pass
+        if not guess["success"]:
+            pass
+  
+
+        # if there was an error, stop the game
+        if guess["error"]:
+            print("ERROR: {}".format(guess["error"]))
+
+        # determine if guess is correct and if any attempts remain
+        recieved_speech = guess["transcription"]
+
+        found = False
+        # if recieved_speech:
+            # print("Message recieved".format(word))
+            # print(recieved_speech)
+            
+        
+
+        for i in COCO_INSTANCE_CATEGORY_NAMES:
+            if i == recieved_speech:
+                # print(i)
+                found = True
+                print("detected: {}".format(recieved_speech))
+        
+        if found == False:
+            print("item not in dataset")
+                
+
+
+
+        
+
+        
+    
+
