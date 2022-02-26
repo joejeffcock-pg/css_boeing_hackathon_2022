91e9bcff8badba3496d39039f1cf97f9436f44be
MaxT098 <92232042+MaxT098@users.noreply.github.com>
2022-02-26 15:26:41 +0000

    Add files via upload

--- communication/Signal2buzzer.ino
+++ communication/Signal2buzzer.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_CircuitPlayground.h>
 
-int score = 0;
+int score = 10;
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,20 +12,7 @@ void loop() {
   //analogWrite(5, 100);
   //get score from serial
   while (score>0) {
-    CircuitPlayground.setPixelColor(0, 255, 0, 0);
-    CircuitPlayground.setPixelColor(1, 255, 0, 0);
-    CircuitPlayground.setPixelColor(2, 255, 0, 0);
-    CircuitPlayground.setPixelColor(3, 255, 0, 0);
-    CircuitPlayground.setPixelColor(4, 255, 0, 0);
-    CircuitPlayground.setPixelColor(5, 255, 0, 0);
-    CircuitPlayground.setPixelColor(6, 255, 0, 0);
-    CircuitPlayground.setPixelColor(7, 255, 0, 0);
-    CircuitPlayground.setPixelColor(8, 255, 0, 0);
-    CircuitPlayground.setPixelColor(9, 255, 0, 0);
-    delay(1000/score);
-    //CircuitPlayground.playTone(440, 500);
-    //CircuitPlayground.playTone(0, 500);
-    CircuitPlayground.clearPixels();
-    delay(1000/score);
+    CircuitPlayground.playTone(440, 1000/score);
+    CircuitPlayground.playTone(0, 1000/score);
     }
 }
